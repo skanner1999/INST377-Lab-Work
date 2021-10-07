b1863bb57252e63e2bf488aff9d34530824b50b8
skanner2 <63942471+skanner2@users.noreply.github.com>
2021-10-07 16:51:08 -0400

    final commit

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,10 +1,8 @@
 async function windowActions() {
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  const cities = [];
+  const request = await fetch(endpoint);
+  const cities = await request.json()
 
-  fetch(endpoint)
-    .then((blob) => blob.json())
-    .then((data) => cities.push(...data));
 
   function findMatches(wordToMatch, cities) {
     return cities.filter((place) => {
@@ -13,21 +11,21 @@ async function windowActions() {
     });
   }
 
-  function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, cities);
     const html = matchArray.map((place) => {
-      const regex = new RegExp(this.value, 'gi');
-      const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`);
+      const regex = new RegExp(event.target.value, 'gi');
+      const cityName = place.city.replace(regex, `<span class="h1">${event.target.value.toUpperCase()}</span>`);
       return `
                 <li>
-                    <span class="name">${place.name}<br>${place.address_line_1}<br>${cityName} ${place.zip}</span>
+                    <span class="name">${place.name}<br>${place.category}<br><em>${place.address_line_1}<br>${cityName}</em><br>${place.zip}</span>
                 </li>
                 `;
     }).join(',');
     if (searchInput.value == '') {
       suggestions.innerHTML = '';
     } else {
-      suggestions.innerHTML = html;
+      suggestions.innerHTML = html
     }
   }
 
@@ -35,6 +33,6 @@ async function windowActions() {
   const suggestions = document.querySelector('.suggestions');
 
   searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt) });
 }
 window.onload = windowActions;
